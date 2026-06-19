354ba7346fb2682583ad332340cb1065872f8745
ying0930 <tavricccc@gmail.com>
2026-06-20 02:36:06 +0800

    Rename blog pages to articles

--- src/data/navLinks.ts
+++ src/data/navLinks.ts
@@ -1,6 +1,6 @@
 export const getNavLinks = (base: string) => [
 	{ href: `${base}`, label: 'Home' },
-	{ href: `${base}blog/`, label: 'Blog' },
+	{ href: `${base}blog/`, label: 'Articles' },
 	{ href: `${base}tags/`, label: 'Tags' },
 	{ href: `${base}links/`, label: 'Links' },
 	{ href: `${base}about/`, label: 'About' },
